c0894df4c67a09745ab13a7ce84bfde0d57ac565
michidevv <michi.devv@gmail.com>
2023-07-31 18:10:57 +0200

    feat: add 701

---
+++ src/701-InsertintoBST.js
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+    if (!root) return new TreeNode(val);
+
+    if (root.val < val) {
+        if (!root.right) {
+            root.right = new TreeNode(val);
+        } else {
+            insertIntoBST(root.right, val);
+        }
+    }
+
+    if (root.val >= val) {
+        if (!root.left) {
+            root.left = new TreeNode(val);
+        } else {
+            insertIntoBST(root.left, val);
+        }
+    }
+
+    return root;
+};
